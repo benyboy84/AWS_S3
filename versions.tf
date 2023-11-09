c0f48b75807b74aae3d3d58d8c0623bf8f1b59e1
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-11-09 14:28:16 -0500

    v1.0.0

--- versions.tf
+++ versions.tf
@@ -7,6 +7,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.23.1"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    } 
   }
 
   required_version = "> 1.3.1"
